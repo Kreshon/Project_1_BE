75932368f614d9bd32f7fbf88d1a255bf00fd3da
Nick Kreshon <nickkree@gmail.com>
2022-01-29 22:20:38 -0500

    saturday

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     next();
   });
 

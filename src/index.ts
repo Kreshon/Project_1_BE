140af79ad5b79f994ffd294efd21b49d338f11b3
Nick Kreshon <nickkree@gmail.com>
2022-01-30 19:46:14 -0500

    fixed style

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,15 @@ const userService: UserService = new UserService(userDao);
 const reimbursementDao: ReimbursementDao = new ReimbursementDao();
 const reimbursementService: ReimbursementService = new ReimbursementService(reimbursementDao);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//     next();
+//   });
 
 app.patch("/login", async (req,res) =>{
     console.log(req.body)
